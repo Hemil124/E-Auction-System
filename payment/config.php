b48bbd4c02b2ae4a0baeea16ec01126edd2ac2c5
KrishVirani <viranikrish46@gmail.com>
2024-12-25 19:58:48 +0530

    Update config.php

--- payment/config.php
+++ payment/config.php
@@ -2,9 +2,9 @@
 
 define('BASE_URL', 'http://localhost/E-Auction-System');
 
-define('API_KEY', 'rzp_test_Gu3YzpwsCudp5k');
+define('API_KEY', '');
 
-define('API_SECRET', 'W7gL3edLsnQlGEJ7PWoJwJBa');
+define('API_SECRET', '');
 
 define('COMPANY_NAME', 'E-Auction');
 
